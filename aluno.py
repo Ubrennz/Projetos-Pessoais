389ed592f646b134326ef629ff412fdd363787bb
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2025-01-02 21:24:11 -0300

    Sistema de notas para salvar em arquivo.csv

--- aluno.py
+++ aluno.py
@@ -1,21 +1,31 @@
 class Aluno:
-    def __init__(self, loops):
-        self.loops = loops
+    def __init__(self, nome, nota1, nota2, nota3):        
+        self.nome = nome
+        self.nota1 = nota1
+        self.nota2 = nota2
+        self.nota3 = nota3
 
 
     def calcular_notas(self):      
+        import csv
         try:
-            soma = 0
-            count = 0
+            media = (self.nota1 + self.nota2) / 3   
+            lista = []            
+            lista.append(self.nome)
+            lista.append(self.nota1)
+            lista.append(self.nota2)
+            lista.append(self.nota3)             
+            lista.append(f'{media:.2f}')
+            print(lista)
 
-            for c in range(0, self.loops):
-                notas = int(input(f'nota {c}ª: '))
-                soma += notas
-                count += 1
+            with open('notas_alunos.csv', 'a', encoding='utf-8', newline='') as arquivo_csv:
+                notas_aluno = csv.writer(arquivo_csv)
+                notas_aluno.writerow(lista)
 
-            print(f'Medía: {soma / count:.2f}')
-        except:
-            print('Erro ao tentar calcular a nota.')
+            print(f'Medía: {media:.2f}')
+        except Exception as erro:
+            print(erro)
+
 
 def linha(count):
     linhas = '-' * count
@@ -30,18 +40,25 @@ def centralizar(posicao, texto):
 print(linha(56))
 print(centralizar(56, 'Sistema de Notas'))
 while True:
-    try:
+    try:        
         print(linha(56))
-        loop = int(input('Digite quantas notas vão ser calculadas ou 0 para sair: '))
+        nome_aluno = input('Digite o nome do aluno ou digite 0 para sair: ')
         print(linha(56))
-        
-        if loop == 0:
-            break
 
-        aluno = Aluno(loop)
-        aluno.calcular_notas()
+        if nome_aluno == '0':
+            break
+        elif isinstance(nome_aluno, str):
+            if nome_aluno.isdigit():            
+                print(f'{nome_aluno} não é um nome valido!')
+            else:
+                nota1 = int(input('Digite a primeira nota: '))
+                print(linha(56))
+                nota2 = int(input('Digite a segunda nota: '))
+                print(linha(56))
+                nota3 = int(input('Digite a terceira nota: '))
+                print(linha(56))
+                aluno = Aluno(nome_aluno, nota1, nota2, nota3)
+                aluno.calcular_notas()
+                aluno.ver_notas()                    
     except ValueError:
         print('Digite um número inteiro!')
-    
-# fazer um arquivo para ter as notas dos alunos, as medias e puder editar as notas e a media
-# e remover as notas e as medias dos alunos
